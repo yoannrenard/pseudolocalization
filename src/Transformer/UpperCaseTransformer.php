2347b13f27e8b06b0e10b90f24d23b35c1c09a46
Yoann Renard <renard.yoann@gmail.com>
2018-04-04 15:36:52 +0200

    Upper Case

---
+++ src/Transformer/UpperCaseTransformer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace YoannRenard\Pseudolocalization\Transformer;
+
+class UpperCaseTransformer implements TransformerInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function transform($message)
+    {
+        return strtoupper($message);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getName()
+    {
+        return 'upper_case';
+    }
+}
