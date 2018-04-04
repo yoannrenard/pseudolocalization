d8cac6b414202031e0a7e716f7073ed0b7b3e55d
Yoann Renard <renard.yoann@gmail.com>
2018-04-04 15:31:38 +0200

    Alternate Case

---
+++ src/Transformer/AlternateCaseTransformer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace YoannRenard\Pseudolocalization\Transformer;
+
+class AlternateCaseTransformer implements TransformerInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function transform($message)
+    {
+        $transformedMessage = '';
+
+        foreach (str_split($message) as $key => $letter) {
+            $transformedMessage .= 0 == $key%2? strtoupper($letter) : $letter;
+        }
+
+        return $transformedMessage;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getName()
+    {
+        return 'alternate_case';
+    }
+}
