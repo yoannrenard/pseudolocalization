f4df27bc92eab5c2714aea5498e1e296dc5f064f
Yoann Renard <renard.yoann@gmail.com>
2018-04-04 15:17:44 +0200

    Enclose in brackets

---
+++ src/Transformer/EncloseInBracketsTransformer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace YoannRenard\Pseudolocalization\Transformer;
+
+class EncloseInBracketsTransformer implements TransformerInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function transform($message)
+    {
+        return sprintf('[%s]', $message);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getName()
+    {
+        return 'enclose_with_bracket';
+    }
+}
